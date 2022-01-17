5f8003ecea731affa8beadf64bc03f971222bea7
Lionel Guglielmone <lionel@guglielmone.com>
2022-01-17 13:25:41 -0300

    webpages with routing

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -2,58 +2,63 @@ const data = {
     products:[
         {
             _id: '1',
-            name: 'A New Dawn',
+            name: ' A New Dawn',
             category: 'book',
             image: '/images/p1.jpg',
             price: 25.3,
-            brand: 'Lucas films',
+            countInStock: 500,
+            brand: ' Lucas films',
             rating: 4.5,
             numReviews: 200,
-            description:'A New Dawn is a canon novel written by John Jackson Miller, with a foreword by Dave Filoni.'
+            description:' A New Dawn is a canon novel written by John Jackson Miller, with a foreword by Dave Filoni.'
         },
         {
             _id: '2',
-            name: 'Force Collector',
+            name: ' Force Collector',
             category: 'book',
             image: '/images/p3.jpg',
             price: 22.3,
-            brand: 'Lucas films',
-            rating: 4,
+            countInStock: 100,
+            brand: ' Lucas films',
+            rating: 2.3,
             numReviews: 300,
-            description:'Force Collector is a canon novel written by Kevin Shinick.'
+            description:' Force Collector is a canon novel written by Kevin Shinick.'
         },
         {
             _id: '3',
-            name: 'Light of the Jedi',
+            name: ' Light of the Jedi',
             category: 'book',
             image: '/images/p4.jpg',
             price: 31.1,
-            brand: 'Lucas films',
-            rating: 5,
+            countInStock: 0,
+            brand: ' Lucas films',
+            rating: 1,
             numReviews: 2200,
-            description:'Light of the Jedi is a canon novel written by Charles Soule.'
+            description:' Light of the Jedi is a canon novel written by Charles Soule.'
         },
         {
             _id: '4',
-            name: 'Apocalypse',
+            name: ' Apocalypse',
             category: 'book',
             image: '/images/p11.jpg',
             price: 11.3,
-            brand: 'Lucas films',
+            countInStock: 5500,
+            brand: ' Lucas films',
             rating: 4.5,
             numReviews: 500,
-            description:'Apocalyse is a SW-Legends novel written by Troy Denning.'
+            description:' Apocalyse is a SW-Legends novel written by Troy Denning.'
         },
         {
             _id: '5',
-            name: 'Into the Void',
+            name: ' Into the Void',
             category: 'book',
             image: '/images/p6.jpg',
             price: 9.99,
-            brand: 'Lucas films',
+            countInStock: 0,
+            brand: ' Lucas films',
             rating: 3.5,
             numReviews: 2200,
-            description:'Into the Void is a canon novel written by Tim Lebon.'
+            description:' Into the Void is a canon novel written by Tim Lebon.'
         }
       
 
