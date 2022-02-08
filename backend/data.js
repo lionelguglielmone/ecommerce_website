2fc08ad056a3af3985e32ddacc76aa9647b24ae4
Lionel Guglielmone <lionel@guglielmone.com>
2022-02-08 14:49:50 -0300

    products added to DB + APIs

--- backend/data.js
+++ backend/data.js
@@ -16,7 +16,6 @@ const data = {
   ],
   products: [
     {
-      _id: "1",
       name: " A New Dawn",
       category: "book",
       image: "/images/p1.jpg",
@@ -29,7 +28,6 @@ const data = {
         " A New Dawn is a canon novel written by John Jackson Miller, with a foreword by Dave Filoni.",
     },
     {
-      _id: "2",
       name: " Force Collector",
       category: "book",
       image: "/images/p3.jpg",
@@ -42,7 +40,6 @@ const data = {
         " Force Collector is a canon novel written by Kevin Shinick.",
     },
     {
-      _id: "3",
       name: " Light of the Jedi",
       category: "book",
       image: "/images/p4.jpg",
@@ -55,7 +52,6 @@ const data = {
         " Light of the Jedi is a canon novel written by Charles Soule.",
     },
     {
-      _id: "4",
       name: " Apocalypse",
       category: "book",
       image: "/images/p11.jpg",
@@ -67,7 +63,6 @@ const data = {
       description: " Apocalyse is a SW-Legends novel written by Troy Denning.",
     },
     {
-      _id: "5",
       name: " Into the Void",
       category: "book",
       image: "/images/p6.jpg",
